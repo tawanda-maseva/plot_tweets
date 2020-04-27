7a544a8502de8f902bda28142d4d91bd5f02714e
tawanda-maseva <56193179+tawanda-maseva@users.noreply.github.com>
2020-04-27 10:21:12 +0200

    removed outdated comments

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from tw_visualizations import Timeline, Tweets_by, Hashtag
 import data_processing as dp
 
-
 tawanda_credentials = '/Users/user/Desktop/python_work/CHAPTER_02/04_working_with_APIs/twitter_project/twitter_credentials.json'
-#timmy = Timeline('timmy', tawanda_credentials)
-#elonmusk = Tweets_by('elonmusk', tawanda_credentials)
-# trump = Tweets_by('realDonaldTrump', tawanda_credentials)
-
-#got = Hashtag('#ForTheThrone', tawanda_credentials)
 
 def main():
 	'''View tweets by likes or retweets'''
@@ -66,37 +60,6 @@ def main():
 
 		dp.welcome_msg()
 		user_input = dp.verify_user_input()
-
-
-
-
-
-
-
-
-
-
-
-
-		#got.tweetsby('likes')
-		#got.tweetsby('retweets')
-
-		#elonmusk.account_tweetsby('likes', 'red')
-		#elonmusk.account_tweetsby('retweets', 'yellow')
-
-		#trump.account_tweetsby('likes')
-		#trump.account_tweetsby('retweets')
-
-		#next_click = input('Refresh or quit? (r/q): ')
-		#if next_click == 'q':
-		#	show = False  # exit
-		#elif next_click == 'r':
-		#	timmy.refresh_tweets()
-			#elonmusk.refresh_tweets()
-			#trump.refresh_tweets()
-			#got.refresh_tweets()
-		#else:
-		#	print('Invalid input')
 
 main()
 
